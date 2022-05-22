207d93d98286fadc089df8226a9acfd3cf6dc0ed
nader31 <nader.chebbo@gmail.com>
2022-05-22 23:14:34 +0200

    Search component update

--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -10,19 +10,29 @@ function SearchComponent() {
             <h4 className='search-h4'>France</h4>
         </div>
         <div className="search-element">
-            <h3 className='search-h3'>VILLE</h3>
-            <h2 className='search-h2'>Monaco</h2>
+            <h3 className='search-h3'>PERSONNES</h3>
+            <h2 className='search-h2'>4 personnes</h2>
             <div className="minus-plus">
                 <h4 className='search-h4'>moins</h4>
                 <h4 className='search-h4'>plus</h4>
             </div>
         </div>
         <div className="search-element">
-            <h3 className='search-h3'>Arrivée</h3>
-            <h2 className='search-h2'>Ven 14 Jan</h2>
-            <div className="minus-plus">
-                <h4 className='search-h4'>moins</h4>
-                <h4 className='search-h4'>plus</h4>
+            <div className="search-dates">
+                <h3 className='search-h3'>ARRIVEE</h3>
+                <h2 className='search-h2'>Ven 14 Jan</h2>
+                <div className="minus-plus">
+                    <h4 className='search-h4'>moins</h4>
+                    <h4 className='search-h4'>plus</h4>
+                </div>
+            </div>
+            <div className="search-dates">
+                <h3 className='search-h3'>DEPART</h3>
+                <h2 className='search-h2'>Lun 17 Jan</h2>
+                <div className="minus-plus">
+                    <h4 className='search-h4'>moins</h4>
+                    <h4 className='search-h4'>plus</h4>
+                </div>
             </div>
         </div>
     </div>
