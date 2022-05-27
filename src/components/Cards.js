99f9363b5b01751b5dcfa309d688f8c2fd82e8d2
nader31 <nader.chebbo@gmail.com>
2022-05-27 19:04:24 +0200

    Clean house cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,8 +1,23 @@
-import React from 'react';
 import CardItem from './CardItem';
 import './Cards.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function Cards(props) {
+  const [houses, setHouses] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:8081/housingApi/getAllHousings')
+      .then((res) => {
+        console.log(res);
+        setHouses(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div className="cards">
       <h1 className="decouvrezLogements">
@@ -10,15 +25,38 @@ function Cards(props) {
       </h1>
       <div className="cards__container">
         <div className="card__wrapper">
-          <ul className="cards__items">
-            <CardItem
-              src="images/img-home.jpg"
-              text="Découvrez cette magnifique propriété située dans le centre de Sydney"
-              label="Maison"
-              path="/Reservations"
-              searchedCity2={props.searchedCity}
-              searchedBeds2={props.searchedBeds}
-            />
+          <ul className="cards__item">
+            {houses
+              .filter((house) => {
+                if (
+                  (props.searchedCity == '' || props.searchedCity == '...') &&
+                  props.searchedBeds == 1
+                ) {
+                  return house;
+                } else if (
+                  (house.city
+                    .toLowerCase()
+                    .includes(props.searchedCity.toLowerCase()) ||
+                    props.searchedCity == '...') &&
+                  house.numberOfBed >= props.searchedBeds
+                ) {
+                  return house;
+                }
+              })
+              .map((house) => (
+                <CardItem
+                  id={house.id}
+                  src={`images/${house.housingPictures[0].fileLocalisation}`}
+                  text="Découvrez cette magnifique propriété située dans le centre de Sydney"
+                  label="Maison"
+                  path="/Reservations"
+                  description={house.description}
+                  city={house.city}
+                  country={house.country}
+                  numberOfBed={house.numberOfBed}
+                  rating={4}
+                />
+              ))}
           </ul>
         </div>
       </div>
