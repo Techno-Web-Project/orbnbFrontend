1f2e23c0088f65e4b64f5a22448575d4bcb84948
MorganDelange <74045908+MorganDelange@users.noreply.github.com>
2022-05-30 07:58:41 +0200

    basic footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,19 @@ import React from 'react';
 import './Footer.css';
 
 function Footer() {
-  return <div className="footerDiv">footer</div>;
+  return <div className="footerDiv">
+    <div>
+      <h3>Nous contacter</h3><br></br>
+      04.75.68.23.14<br></br>
+      contact@orbnb.fr
+      </div>
+    <div><h3>Nous retrouver</h3><br></br>
+      <img class="logoFooter" src = "images\logo_facebook.png"></img>  <p class="pFooter">Orbnb</p><br></br><br></br>
+      <img class="logoFooter" src = "images\logo_instagram.png"></img> <p class="pFooter">@orbnb_official</p><br></br><br></br>
+      <img class="logoFooter" src = "images\logo_twitter.png"></img> <p class="pFooter">@orbnb</p><br></br><br></br>
+    </div>
+
+  </div>;
 }
 
 export default Footer;
